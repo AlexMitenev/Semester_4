999be19cb8e19fa09094790528158ca745d70c0c
Alex Mitenev <alexmitenev@gmail.com>
2014-03-20 21:24:07 +0400

    first white app

---
+++ White/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.White;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.MenuItems;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White.UIItems.WindowStripControls;
+using NUnit.Framework;
+using NUnit;
+
+namespace WhiteLib
+{
+    [TestFixture]
+    public class Sample2
+    {
+        static void Main() { }
+
+        [Test]
+        public void TestMethod()
+        {
+            Application app = null;
+            try
+            {
+
+                app = Application.Launch("TestWPF.exe");
+                Window window = app.GetWindow("MainWindow");
+
+                var button = window.Get<Button>("btnOK");
+                button.Click();
+
+                Assert.AreEqual(window.Enabled, true);
+            }
+            finally
+            {
+                app.Close();
+            }
+        }
+    }
+}
